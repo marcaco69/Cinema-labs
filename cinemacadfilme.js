9d13b842b0b4a908b7653d51207f95eca50ba0f8
Marcaco <marco211104@outlook.com>
2025-04-11 22:48:56 -0300

    Commit 2

--- cinemacadfilme.js
+++ cinemacadfilme.js
@@ -3,17 +3,19 @@ function CadastroFilme() {
     let titulo = CadastroTitulo.value.trim();
     let CadastroDescricao = document.getElementById("Descricao");
     let descricao = CadastroDescricao.value;
+    let CadastroGenero = document.getElementById("Genero");
+    let genero = CadastroGenero.value;
 
     if(titulo == "") {
         alert("Digite os dados do filme");
     }
 
-    else{
-    let listadefilmes = document.getElementById("listadefilmes");
-    let novofilme = document.createElement("li");
-    novofilme.textContent = titulo;
-    listadefilmes.appendChild(novofilme);
-    }
+    else{ //apagar (só teste)
+        let listadefilmes = document.getElementById("listadefilmes");
+        let novofilme = document.createElement("li");
+        novofilme.textContent = titulo;
+        listadefilmes.appendChild(novofilme);
+        } // até aqui
 
     CadastroTitulo.value = "";
     CadastroDescricao.value = "";
